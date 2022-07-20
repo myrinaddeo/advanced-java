df6748c01e6517214bddbf2d1d06598555a56cc0
Myriam Naddeo <myrinaddeo@MacBook-Air-de-Myri.local>
2022-07-20 15:19:50 -0300

    ejemplos

--- sqliteIJExample/src/main/java/org/example/App.java
+++ sqliteIJExample/src/main/java/org/example/App.java
@@ -8,45 +8,36 @@ public class App {
         int[] ids = {0,1,2};
         String[] names = {"Mary", "Bob", "Charlie"};
 
-        Class.forName("org.sqlite.JDBC");
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String dbUrl = "jdbc:mysql://localhost:3306/people";
 
-        String dbUrl = "jdbc:sqlite:people.db";
-
-        var conn = DriverManager.getConnection(dbUrl);
+        var conn = DriverManager.getConnection(dbUrl, "root", "2012mayomis");
 
         var stmt = conn.createStatement();
         conn.setAutoCommit(false);
 
-        var sql = "Create table if not exists user (id integer primary key, name text not null)";
-        stmt.execute(sql);
-
-        sql ="insert into user (id, name) values (?,?)";
+        var sql ="insert into user (id, name) values (?,?)";
         var insertStmt = conn.prepareStatement(sql);
 
         for(int i=0; i<ids.length;i++){
             insertStmt.setInt(1,ids[i]);
             insertStmt.setString(2,names[i]);
 
-            //insertStmt.executeUpdate();
+            insertStmt.executeUpdate();
         }
-
         conn.commit();
-
-
         insertStmt.close();
 
         sql = "select id, name from user";
         var rs =stmt.executeQuery(sql);
 
         while (rs.next()){
-            int id = rs.getInt("id");
-            String name = rs.getString("name");
+            int id = rs.getInt("Id");
+            String name = rs.getString("Name");
+
             System.out.println(id + ": " + name);
         }
 
-
-        sql = "drop table user";
-        //stmt.execute(sql);
 
         stmt.close();
         conn.close();
